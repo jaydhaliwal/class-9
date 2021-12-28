3c5c1bb5f74a241bab79ee7ac24b5c4df21a2b61
jaydhaliwal <94186548+jaydhaliwal@users.noreply.github.com>
2021-12-28 11:16:06 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+  createCanvas(400,400);
+  sprite = createSprite(100,100,100,100)
+sprite.shapeColor="blue"
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+}
+
+
+
+
