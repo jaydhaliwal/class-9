e235fa355a6027c3825390123e3753ed55ee4fb1
jaydhaliwal <94186548+jaydhaliwal@users.noreply.github.com>
2021-12-28 15:43:50 +0000

    Add files via upload

---
+++ class 9 homework.js
@@ -0,0 +1,33 @@
+var Box;
+
+function setup(){
+createCanvas(400,400)
+box = createSprite(200,200,30,30)
+
+}
+  
+  function draw() 
+  {
+  background(30);
+
+if(keyIsDown(RIGHT_ARROW))
+{
+box.position.x = box.posion.x +5
+}
+
+if(keyIsDown(LEFT_ARROW))
+{
+box.position.x = box.posion.x -5 
+}
+if(keyIsDown(UP_ARROW))
+{
+box.position.y = box.posion.y -5
+}
+
+if(keyIsDown(LEFT_ARROW))
+{
+box.position.y = box.posion.y +5
+}
+drawSprites()
+
+}
